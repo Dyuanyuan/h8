a5d797eed67f1c5ac48272ab0c10b457fd26afd5
student <student@iMac-71.local>
2018-12-13 18:13:17 +0800

    kk

--- h8/ViewController.swift
+++ h8/ViewController.swift
@@ -8,13 +8,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+   
+    
+    
+    @IBOutlet weak var chooseLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var nameTextField: UITextField!
+    var stuArray = [String]()
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stuArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StudentCell") as! StudentTableViewCell
+        
+        cell.name.text = stuArray[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 88
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        chooseLabel.text = "You choose: \(stuArray[indexPath.row])"
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            stuArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let name = stuArray.remove(at: sourceIndexPath.row)
+        stuArray.insert(name, at: destinationIndexPath.row)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        stuArray.append("ongseongwu")
+        stuArray.append("kangdaniel")
+        stuArray.append("laikuanlin")
+        stuArray.append("dingyuanyuan")
+        
     }
-
-
+    @IBAction func addClicked(_ sender: Any) { if let name = nameTextField.text {
+        stuArray.append(name)
+        tableView.reloadData()
+        nameTextField.resignFirstResponder()
+        }
+    }
+    
+    @IBAction func editClicked(_ sender: Any) {
+        tableView.isEditing = !tableView.isEditing
+    }
 }
 
